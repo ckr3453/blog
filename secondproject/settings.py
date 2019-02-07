52836e4f46990f2cee1f90624a9e9a2bc85383d9
kyungrok <david3453@naver.com>
2019-02-07 19:45:37 +0900

    add about template extends and home link for portfolio page.

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'secondproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['blog/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
