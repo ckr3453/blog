e013672e80614f949c427d8efb270eeced4a86ca
kyungrok <david3453@naver.com>
2019-02-07 21:51:13 +0900

    add login, logout, signup

--- secondproject/urls.py
+++ secondproject/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('blog/', include('blog.urls')), # blog폴더에 urls파일의 정보를 include 해라. 시작 url은 blog/
                                          # 사용하려면 include를 import 받아야함.
     path('portfolio/', portfolio.views.portfolio, name="portfolio"),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # 병렬적으로 추가 (좀더 효율적, 추후에 학습 필요) (urlpatterns += (~~)와 같음)
