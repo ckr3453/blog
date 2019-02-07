0c3915bc4413013921a19504da348a2008f5bfbc
kyungrok <david3453@naver.com>
2019-02-07 20:19:43 +0900

    url 관리를 위한 url 분리

--- secondproject/urls.py
+++ secondproject/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 import blog.views
 import portfolio.views
 from django.conf import settings
@@ -10,9 +10,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog.views.home, name="home"),
-    path('blog/<int:blog_id>', blog.views.detail, name="detail"), #패스컨버터를 사용하여 정수형숫자로 객체들(블로그 글들)을관리, detail 함수로 blog_id를 넘겨줌
-    path('blog/new/', blog.views.new, name="new"),
-    path('blog/create/', blog.views.create, name="create"),
+    path('blog/', include('blog.urls')), # blog폴더에 urls파일의 정보를 include 해라. 시작 url은 blog/
+                                         # 사용하려면 include를 import 받아야함.
     path('portfolio/', portfolio.views.portfolio, name="portfolio"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # 병렬적으로 추가 (좀더 효율적, 추후에 학습 필요) (urlpatterns += (~~)와 같음)
